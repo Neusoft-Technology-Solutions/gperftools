07d494feae4eb6d107635a61a378ec2468c5fa5f
foss <foss@de.neusoft.com>
2024-08-15 06:58:28 +0200

    Implement TCMalloc_SystemRelease for qnx 7.0

--- src/system-alloc.cc
+++ src/system-alloc.cc
@@ -1,11 +1,11 @@
 // -*- Mode: C++; c-basic-offset: 2; indent-tabs-mode: nil -*-
 // Copyright (c) 2005, Google Inc.
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-// 
+//
 //     * Redistributions of source code must retain the above copyright
 // notice, this list of conditions and the following disclaimer.
 //     * Redistributions in binary form must reproduce the above
@@ -15,7 +15,7 @@
 //     * Neither the name of Google Inc. nor the names of its
 // contributors may be used to endorse or promote products derived from
 // this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -75,6 +75,11 @@
 // MADV_DONTNEED.
 #if !defined(MADV_FREE) && defined(MADV_DONTNEED)
 # define MADV_FREE  MADV_DONTNEED
+#endif
+
+#if !defined(MADV_FREE) && defined(__QNXNTO__) && defined(POSIX_MADV_DISCARD_NP)
+# define MADV_FREE POSIX_MADV_DISCARD_NP
+# define madvise(addr, len, advice) posix_madvise((addr), (len), (advice))
 #endif
 
 // Solaris has a bug where it doesn't declare madvise() for C++.
